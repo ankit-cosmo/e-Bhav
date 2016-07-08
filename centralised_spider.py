8bd9591a531619cc20e2bd651275b5ad73b8ac1d
Ankit Ghildiyal <ankit.waterzoom612@gmail.com>
2016-07-08 11:28:46 +0530

    Update centralised_spider.py

--- centralised_spider.py
+++ centralised_spider.py
@@ -6,29 +6,29 @@ import snapdeal_spider
 import nykaa_spider
 
 pf = open("file.txt")
-a=pf.read()
+a=pf.read()#saves the content of the file in a string
 pf.close()
 
 l=len(a)
 i=0
-q=1
+q=1 #counter for serial no. of product
 name=''
 
 f=open('products.txt','w')
 f.close()
 
-for i in range(0,l):
+for i in range(0,l):#operate on items in the string seperated by newline
 	if(a[i]!='\n'):	
-		name+=a[i]
+		name+=a[i]#append until newline occours
 	else:
 		item_type=name
-		f=open('products.txt','a')		
+		f=open('products.txt','a')#write in file	
 		f.write(str(q))
 		f.write("\t")
 		f.write(item_type)
 		f.write("\n")
 		f.close()
-		flipkart_spider.flipkart_spider(item_type)
+		flipkart_spider.flipkart_spider(item_type)#++++NEED TO MAKE THIS MULTITHREADED++++
 		amazon_spider.amazon_spider(item_type)
 		ebay_spider.ebay_spider(item_type)
 		snapdeal_spider.snapdeal_spider(item_type)
